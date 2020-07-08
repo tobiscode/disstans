Lots of minor changes - create_powerlaw_noise() fixed calculation method - regularize parameter now set by default on spline classes - scalogram default color range now uses maximum of data - Arctangent model bugfix and scaling - station.py line lengths - Station.add_local_model_dict() added - analyze_residuals() now has verbose option

geonat/models.py
@@ -524,7 +524,7 @@ class BSpline(Model):
        Society for Industrial and Applied Mathematics.
        doi:`10.1137/1.9781611970555 <https://doi.org/10.1137/1.9781611970555>`_
     """
-    def __init__(self, degree, scale, t_reference,
+    def __init__(self, degree, scale, t_reference, regularize=True,
                  time_unit="D",num_splines=1, spacing=None, **model_kw_args):
         self.degree = int(degree)
         """ Degree :math:`p` of the B-Splines. """
@@ -555,7 +555,7 @@ class BSpline(Model):
                                       + pd.Timedelta(self.scale, time_unit)
                                       * (self.degree + 1)/2).isoformat()
         super().__init__(num_parameters=num_splines, t_reference=t_reference,
-                         time_unit=time_unit, **model_kw_args)
+                         time_unit=time_unit, regularize=regularize, **model_kw_args)
 
     def _get_arch(self):
         arch = {"type": "BSpline",
@@ -613,7 +613,7 @@ class ISpline(Model):
     --------
     geonat.model.Bspline : More details about B-Splines.
     """
-    def __init__(self, degree, scale, t_reference,
+    def __init__(self, degree, scale, t_reference, regularize=True,
                  time_unit="D", num_splines=1, spacing=None, zero_after=False, **model_kw_args):
         self.degree = int(degree)
         """ Degree :math:`p` of the B-Splines. """
@@ -644,7 +644,8 @@ class ISpline(Model):
                                       + pd.Timedelta(self.scale, time_unit)
                                       * (self.degree + 1)/2).isoformat()
         super().__init__(num_parameters=num_splines, t_reference=t_reference,
-                         time_unit=time_unit, zero_after=zero_after, **model_kw_args)
+                         time_unit=time_unit, zero_after=zero_after,
+                         regularize=regularize, **model_kw_args)
 
     def _get_arch(self):
         arch = {"type": "ISpline",
@@ -730,7 +731,7 @@ class SplineSet(Model):
     """
     def __init__(self, degree, t_center_start, t_center_end,
                  time_unit="D", list_scales=None, list_num_knots=None,
-                 splineclass=ISpline, complete=True, **model_kw_args):
+                 splineclass=ISpline, complete=True, regularize=True, **model_kw_args):
         assert np.logical_xor(list_scales is None, list_num_knots is None), \
             "To construct a set of Splines, pass exactly one of " \
             "'list_scales' and 'list_num_knots' " \
@@ -773,10 +774,11 @@ class SplineSet(Model):
             t_ref = t_center_start_tstamp - num_overlaps*scale_tdelta
             # create model and append
             splset.append(splineclass(degree, scale_float, num_splines=num_centerpoints,
-                          t_reference=t_ref, time_unit=time_unit))
+                          t_reference=t_ref, time_unit=time_unit, regularize=regularize))
         # create the actual Model object
         super().__init__(num_parameters=num_parameters, time_unit=time_unit,
-                         zero_after=False if splineclass == ISpline else True, **model_kw_args)
+                         zero_after=False if splineclass == ISpline else True,
+                         regularize=regularize, **model_kw_args)
         self.degree = degree
         """ Degree of the splines. """
         self.t_center_start = t_center_start
@@ -890,14 +892,16 @@ class SplineSet(Model):
                 "'cmaprange' must be None or a single float or integer of the " \
                 f"one-sided color range of the scalogram, got {cmaprange}."
         else:
-            cmaprange = np.percentile(np.concatenate([np.abs(model.parameters)
-                                                      for model in self.splines],
-                                                     axis=0).ravel(), 95)
+            cmaprange = np.max(np.concatenate([np.abs(model.parameters)
+                                               for model in self.splines],
+                                              axis=0).ravel())
         cmap = mpl.cm.ScalarMappable(cmap=scm.seismic,
                                      norm=mpl.colors.Normalize(vmin=-cmaprange,
                                                                vmax=cmaprange))
         # start plotting
-        fig, ax = plt.subplots(nrows=3, sharex=True)
+        fig, ax = plt.subplots(nrows=num_components, sharex=True)
+        if num_components == 1:
+            ax = [ax]
         for i, model in enumerate(self.splines):
             # where to put this scale
             y_off = 1 - (i + 1)*dy_scale
@@ -1045,7 +1049,8 @@ class Arctangent(Model):
     Subclasses :class:`~geonat.model.Model`.
 
     This model provides the arctangent :math:`\arctan(\mathbf{t}/\tau)`,
-    stretched with a given time constant.
+    stretched with a given time constant and normalized to approach
+    :math:`\pm 1` at the limits.
 
     Because this model is always transient, it is recommended not to
     use it in the estimation of parameters, even when using ``t_start``
@@ -1077,5 +1082,5 @@ class Arctangent(Model):
 
     def _get_mapping(self, timevector):
         dt = self.tvec_to_numpycol(timevector)
-        coefs = np.atan(dt / self.tau).reshape(-1, 1)
+        coefs = np.arctan(dt / self.tau).reshape(-1, 1) / (np.pi/2)
         return coefs

geonat/station.py
@@ -85,7 +85,9 @@ class Station():
         """
         info = f"Station {self.name} at {self.location} with timeseries"
         for ts_description, ts in self.timeseries.items():
-            info += f"\n[{ts_description}]\n - Source: {ts.src}\n - Units: {ts.data_unit}\n" + \
+            info += f"\n[{ts_description}]\n" \
+                    f" - Source: {ts.src}\n" \
+                    f" - Units: {ts.data_unit}\n" + \
                     f" - Data: {[key for key in ts.data_cols]}\n" + \
                     f" - Uncertainties: {[key for key in ts.sigma_cols]}"
             if len(self.models[ts_description]) > 0:
@@ -150,7 +152,8 @@ class Station():
 
         Example
         -------
-        If ``stat`` is a :class:`~Station` instance, then the following two loops are equivalent::
+        If ``stat`` is a :class:`~Station` instance,then the following two loops
+        are equivalent::
 
             # long version
             for ts in stat.timeseries.values():
@@ -176,11 +179,14 @@ class Station():
 
         See Also
         --------
-        geonat.network.Network.to_json : Export the Network configuration as a JSON file.
+        geonat.network.Network.to_json
+            Export the Network configuration as a JSON file.
         geonat.timeseries.Timeseries.get_arch
-            Get the architecture dictionary of a :class:`~geonat.timeseries.Timeseries` instance.
+            Get the architecture dictionary of a
+            :class:`~geonat.timeseries.Timeseries` instance.
         geonat.model.Model.get_arch
-            Get the architecture dictionary of a :class:`~geonat.model.Model` instance.
+            Get the architecture dictionary of a
+            :class:`~geonat.model.Model` instance.
         """
         # create empty dictionary
         stat_arch = {"location": self.location,
@@ -193,11 +199,12 @@ class Station():
                 stat_arch["timeseries"].update({ts_description: ts_arch})
             if len(self.models[ts_description]) > 0:
                 stat_arch["models"][ts_description] = {}
-                for mdl_description, mdl in self.models[ts_description].items():
-                    stat_arch["models"][ts_description].update({mdl_description: mdl.get_arch()})
+                for mdl_desc, mdl in self.models[ts_description].items():
+                    stat_arch["models"][ts_description].update({mdl_desc: mdl.get_arch()})
         return stat_arch
 
-    def add_timeseries(self, ts_description, timeseries, override_src=None, override_data_unit=None,
+    def add_timeseries(self, ts_description, timeseries,
+                       override_src=None, override_data_unit=None,
                        override_data_cols=None, override_sigma_cols=None, add_models=None):
         """
         Add a timeseries to the station.
@@ -212,16 +219,20 @@ class Station():
         timeseries : geonat.timeseries.Timeseries
             Timeseries object to add.
         override_src : str, optional
-            Override the :attr:`~geonat.timeseries.Timeseries.src` attribute of ``timeseries``.
+            Override the :attr:`~geonat.timeseries.Timeseries.src`
+            attribute of ``timeseries``.
         override_data_unit : str, optional
-            Override the :attr:`~geonat.timeseries.Timeseries.data_unit` attribute of ``timeseries``.
+            Override the :attr:`~geonat.timeseries.Timeseries.data_unit`
+            attribute of ``timeseries``.
         override_data_cols : str, optional
-            Override the :attr:`~geonat.timeseries.Timeseries.data_cols` attribute of ``timeseries``.
+            Override the :attr:`~geonat.timeseries.Timeseries.data_cols`
+            attribute of ``timeseries``.
         override_sigma_cols : str, optional
-            Override the :attr:`~geonat.timeseries.Timeseries.sigma_cols` attribute of ``timeseries``.
+            Override the :attr:`~geonat.timeseries.Timeseries.sigma_cols`
+            attribute of ``timeseries``.
         add_models : dict, optional
-            Dictionary of models to add to the timeseries, where the keys are the model description
-            and the values are :class:`~geonat.model.Model` objects.
+            Dictionary of models to add to the timeseries, where the keys are the
+            model description and the values are :class:`~geonat.model.Model` objects.
 
         See Also
         --------
@@ -229,16 +240,19 @@ class Station():
 
         Example
         -------
-        If ``stat`` is a :class:`~Station` instance, ``ts_description`` the ts_description of a new timeseries,
-        and ``ts`` a :class:`~geonat.timeseries.Timeseries` instance, then the following two are equivalent::
+        If ``stat`` is a :class:`~Station` instance, ``ts_description`` the ts_description
+        of a new timeseries, and ``ts`` a :class:`~geonat.timeseries.Timeseries` instance,
+        then the following two are equivalent::
 
             stat.add_timeseries(ts_description, timeseries)
             stat[ts_description] = timeseries
         """
         if not isinstance(ts_description, str):
-            raise TypeError("Cannot add new timeseries: 'ts_description' is not a string.")
+            raise TypeError("Cannot add new timeseries: "
+                            "'ts_description' is not a string.")
         if not isinstance(timeseries, Timeseries):
-            raise TypeError("Cannot add new timeseries: 'timeseries' is not a Timeseries object.")
+            raise TypeError("Cannot add new timeseries: "
+                            "'timeseries' is not a Timeseries object.")
         if ts_description in self.timeseries:
             warn(f"Station {self.name}: Overwriting time series '{ts_description}'.",
                  category=RuntimeWarning)
@@ -257,7 +271,8 @@ class Station():
             for model_description, model_cfg in add_models.items():
                 local_copy = deepcopy(model_cfg)
                 mdl = getattr(geonat_models, local_copy["type"])(**local_copy["kw_args"])
-                self.add_local_model(ts_description=ts_description, model_description=model_description, model=mdl)
+                self.add_local_model(ts_description=ts_description,
+                                     model_description=model_description, model=mdl)
 
     def remove_timeseries(self, ts_description):
         """
@@ -274,14 +289,15 @@ class Station():
 
         Example
         -------
-        If ``stat`` is a :class:`~Station` instance, ``ts_description`` the ts_description of the timeseries
-        to remove, then the following two are equivalent::
+        If ``stat`` is a :class:`~Station` instance, ``ts_description`` the ts_description
+        of the timeseries to remove, then the following two are equivalent::
 
             stat.remove_timeseries(ts_description)
             del stat[ts_description]
         """
         if ts_description not in self.timeseries:
-            warn(f"Station {self.name}: Cannot find time series '{ts_description}', couldn't delete.",
+            warn(f"Station {self.name}: "
+                 f"Cannot find time series '{ts_description}', couldn't delete.",
                  category=RuntimeWarning)
         else:
             del self.timeseries[ts_description]
@@ -309,31 +325,61 @@ class Station():
         if not isinstance(model, Model):
             raise TypeError("Cannot add new local model: 'model' is not a Model object.")
         assert ts_description in self.timeseries, \
-            f"Station {self.name}: Cannot find timeseries '{ts_description}' to add local model '{model_description}'."
+            f"Station {self.name}: " \
+            f"Cannot find timeseries '{ts_description}' to add local model '{model_description}'."
         if model_description in self.models[ts_description]:
-            warn(f"Station {self.name}, timeseries {ts_description}: Overwriting local model '{model_description}'.",
+            warn(f"Station {self.name}, timeseries {ts_description}: "
+                 f"Overwriting local model '{model_description}'.",
                  category=RuntimeWarning)
         self.models[ts_description].update({model_description: model})
 
-    def remove_local_model(self, ts_description, model_description):
+    def add_local_model_dict(self, ts_description, model_dict):
         """
-        Remove a model from a timeseries.
+        Add a dictionary of models to a timeseries (overwrites the models if they have already
+        been added with the same description).
+
+        Wraps :meth:`~add_local_model`.
+
+        Parameters
+        ----------
+        ts_description : str
+            Timeseries to add the model to.
+        model_dict : dict
+            Dictionary of ``{model_description: model}`` key-value pairs to add.
+        """
+        assert isinstance(model_dict, dict), \
+            f"'model_dict' needs to be a dictionary, got {type(model_dict)}."
+        for mdl_desc, mdl in model_dict.items():
+            self.add_local_model(ts_description, mdl_desc, mdl)
+
+    def remove_local_models(self, ts_description, model_descriptions):
+        """
+        Remove models from a timeseries.
 
         Parameters
         ----------
         ts_description : str
             Timeseries to remove the model from.
-        model_description : str
-            Model description.
+        model_descriptions : str or list
+            Model description(s).
         """
-        if ts_description not in self.timeseries:
-            warn(f"Station {self.name}: Cannot find timeseries '{ts_description}', "
-                 f"couldn't delete local model '{model_description}'.", category=RuntimeWarning)
-        elif model_description not in self.models[ts_description]:
-            warn(f"Station {self.name}, timeseries {ts_description}: "
-                 f"Cannot find local model '{model_description}', couldn't delete.", category=RuntimeWarning)
+        # unpack list
+        if isinstance(model_descriptions, str):
+            model_list = [model_descriptions]
         else:
-            del self.models[ts_description][model_description]
+            model_list = model_descriptions
+        # iterate removal
+        for mdl_desc in model_list:
+            if ts_description not in self.timeseries:
+                warn(f"Station {self.name}: Cannot find timeseries '{ts_description}', "
+                     f"couldn't delete local model '{mdl_desc}'.",
+                     category=RuntimeWarning)
+            elif mdl_desc not in self.models[ts_description]:
+                warn(f"Station {self.name}, timeseries {ts_description}: "
+                     f"Cannot find local model '{mdl_desc}', couldn't delete.",
+                     category=RuntimeWarning)
+            else:
+                del self.models[ts_description][mdl_desc]
 
     def add_fit(self, ts_description, model_description, fit):
         """
@@ -364,10 +410,13 @@ class Station():
         if not isinstance(model_description, str):
             raise TypeError("Cannot add new fit: 'model_description' is not a string.")
         assert ts_description in self.timeseries, \
-            f"Station {self.name}: Cannot find timeseries '{ts_description}' to add fit for model '{model_description}'."
+            f"Station {self.name}: Cannot find timeseries '{ts_description}' " \
+            f"to add fit for model '{model_description}'."
         assert model_description in self.models[ts_description], \
-            f"Station {self.name}, timeseries {ts_description}: Cannot find local model '{model_description}', couldn't add fit."
-        data_cols = [ts_description + "_" + model_description + "_" + dcol for dcol in self.timeseries[ts_description].data_cols]
+            f"Station {self.name}, timeseries {ts_description}: " \
+            f"Cannot find local model '{model_description}', couldn't add fit."
+        data_cols = [ts_description + "_" + model_description + "_" + dcol
+                     for dcol in self.timeseries[ts_description].data_cols]
         fit_ts = Timeseries.from_fit(self.timeseries[ts_description].data_unit, data_cols, fit)
         self.fits[ts_description].update({model_description: fit_ts})
         return fit_ts
@@ -385,13 +434,16 @@ class Station():
         """
         if ts_description not in self.timeseries:
             warn(f"Station {self.name}: Cannot find timeseries '{ts_description}', "
-                 f"couldn't delete fit for model '{model_description}'.", category=RuntimeWarning)
+                 f"couldn't delete fit for model '{model_description}'.",
+                 category=RuntimeWarning)
         elif model_description not in self.models[ts_description]:
             warn(f"Station {self.name}, timeseries {ts_description}: "
-                 f"Cannot find local model '{model_description}', couldn't delete fit.", category=RuntimeWarning)
+                 f"Cannot find local model '{model_description}', couldn't delete fit.",
+                 category=RuntimeWarning)
         elif model_description not in self.fits[ts_description]:
             warn(f"Station {self.name}, timeseries {ts_description}: "
-                 f"Cannot find fit for local model '{model_description}', couldn't delete.", category=RuntimeWarning)
+                 f"Cannot find fit for local model '{model_description}', couldn't delete.",
+                 category=RuntimeWarning)
         else:
             del self.fits[ts_description][model_description]
 
@@ -418,7 +470,8 @@ class Station():
         # shorthand for timeseries
         ts = self[ts_description]
         # get model subset
-        fits_to_sum = {model_description: fit for model_description, fit in self.fits[ts_description].items()
+        fits_to_sum = {model_description: fit
+                       for model_description, fit in self.fits[ts_description].items()
                        if (model_list is None) or (model_description in model_list)}
         # sum models and uncertainties
         i_sigma_cols = [i for i, val in enumerate(ts.sigma_cols) if val is not None]
@@ -432,7 +485,8 @@ class Station():
             fit_sum_sigma = np.sqrt(fit_sum_sigma)
         return fit_sum, fit_sum_sigma
 
-    def analyze_residuals(self, ts_description, mean=False, std=False, n_observations=False, std_outlier=0):
+    def analyze_residuals(self, ts_description, verbose=False,
+                          mean=False, std=False, n_observations=False, std_outlier=0):
         """
         Analyze, print and return the residuals of a station's timeseries according
         to certain metrics defined in the arguments.
@@ -441,6 +495,8 @@ class Station():
         ----------
         ts_description : str
             Timeseries to analyze. Method assumes it already is a residual.
+        verbose : bool, optional
+            If True, additionally print the results. Defaults to ``False``.
         mean : bool, optional
             If ``True``, calculate the mean of the timeseries.
             Adds the key ``'Mean'`` to the output dictionary.
@@ -466,22 +522,28 @@ class Station():
             Empty by default.
         """
         assert isinstance(ts_description, str), \
-            f"Station {self.name}: 'ts_description' needs to be a string, got {type(ts_description)}."
-        assert ts_description in self.timeseries, f"Station {self.name}: Can't find '{ts_description}' to analyze."
+            f"Station {self.name}: " \
+            f"'ts_description' needs to be a string, got {type(ts_description)}."
+        assert ts_description in self.timeseries, \
+            f"Station {self.name}: Can't find '{ts_description}' to analyze."
         results = {}
         if mean:
-            results["Mean"] = self[ts_description].data.mean(axis=0, skipna=True, numeric_only=True).values
+            mean = self[ts_description].data.mean(axis=0, skipna=True, numeric_only=True).values
+            results["Mean"] = mean
         if std:
-            results["Standard Deviation"] = self[ts_description].data.std(axis=0, skipna=True, numeric_only=True).values
+            sd = self[ts_description].data.std(axis=0, skipna=True, numeric_only=True).values
+            results["Standard Deviation"] = sd
         if n_observations:
-            results["Observations"] = self[ts_description].data.count(axis=0, numeric_only=True).values
-            results["Gaps"] = self[ts_description].num_observations - results["Observations"]
+            n_obs = self[ts_description].data.count(axis=0, numeric_only=True).values
+            results["Observations"] = n_obs
+            results["Gaps"] = self[ts_description].num_observations - n_obs
         if std_outlier > 0:
             temp = self[ts_description].data.values
             temp[np.isnan(temp)] = 0
             temp -= np.mean(temp, axis=0, keepdims=True)
             temp = temp > np.std(temp, axis=0, keepdims=True) * std_outlier
             results["Outliers"] = np.sum(temp, axis=0, dtype=int)
-        if results:  # only print if any statistic was recorded
-            print(pd.DataFrame(data=results, index=self[ts_description].data_cols).rename_axis(f"{self.name}: {ts_description}", axis=1))
+        if verbose and results:  # only print if any statistic was recorded
+            print_df = pd.DataFrame(data=results, index=self[ts_description].data_cols)
+            print(print_df.rename_axis(f"{self.name}: {ts_description}", axis=1))
         return results

geonat/tools.py
@@ -137,7 +137,8 @@ def create_powerlaw_noise(size, exponent, seed=None):
     Notes
     -----
     This function uses Timmer and König's [timmerkoenig95]_ approach to
-    generate the noise, and is informed by Felix Patzelt's `colorednoise`_ package.
+    generate the noise, and Felix Patzelt's `colorednoise`_ code to calculate
+    the theoretical standard deviation.
 
     References
     ----------
@@ -158,15 +159,28 @@ def create_powerlaw_noise(size, exponent, seed=None):
     # the scaling later can't handle zero frequency, so we need to set it
     # to the minimum frequency possible
     freqs[0] = 1/size
-    # draw one or two sets of Gaussian distributed random numbers
-    # (one if size is even), then multiply them by a
-    # frequency-dependent scaling factor
+    # scale the frequencies
+    freqs_scaled = freqs**(-exponent/2)
+    # draw two sets of Gaussian distributed random numbers
     rng = np.random.default_rng(seed)
-    fourier_noise = rng.standard_normal(halfsize) * freqs**(-exponent/2)
-    if (size % 2) != 0:
-        imag_part = rng.standard_normal(halfsize) * freqs**(-exponent/2)
-        fourier_noise = fourier_noise + imag_part * 1j
+    real_part = rng.standard_normal(halfsize) * freqs_scaled
+    imag_part = rng.standard_normal(halfsize) * freqs_scaled
+    # for real signals, there is no imaginary component at the zero frequency
+    imag_part[0] = 0
+    # for even length signals, the last component (Nyquist frequency)
+    # also has to be real because of symmetry properties
+    if (size % 2) == 0:
+        imag_part[-1] = 0
+    # combine the two parts
+    fourier_noise = real_part + imag_part * 1j
     # step 3
     # transform from frequency to time domain
     noise = np.fft.irfft(fourier_noise, n=size)
+    # additional step: normalize to unit standard deviation
+    # estimate the standard deviation
+    freqs_sigma_est = freqs_scaled[1:].copy()
+    freqs_sigma_est[-1] *= (1 + (size % 2)) / 2
+    sigma = 2 * np.sqrt(np.sum(freqs_sigma_est**2)) / size
+    # normalize
+    noise /= sigma
     return noise