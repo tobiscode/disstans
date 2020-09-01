74f07444882aa17df771cb362df6af7ed7794e4a
tobiscode <47008700+tobiscode@users.noreply.github.com>
2020-08-31 17:26:50 -0700

    use setup.cfg to read __version__ without import

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from numpy.distutils.core import setup, Extension
-from geonat import __version__ as geonat_version
-
-# import README
-with open('README.md') as f:
-    readme = f.read()
 
 # specify that the Fortran code should be included and compiled
 compiled_utils = Extension('geonat.compiled', sources=['geonat/compiled.f90'])
@@ -13,19 +8,5 @@ compiled_utils = Extension('geonat.compiled', sources=['geonat/compiled.f90'])
 pkg_data = {'geonat': ['scm/*.txt', 'scm/LICENSE']}
 
 # run setup
-setup(name='geonat',
-      version=geonat_version,
-      description='Geodetic Network Analysis Tools',
-      long_description=readme,
-      author='Tobias Köhne',
-      author_email='47008700+tobiscode@users.noreply.github.com',
-      url='https://github.com/tobiscode/geonat',
-      python_requires='>=3',
-      license='GNU General Public License v3 (GPLv3)',
-      packages=['geonat'],
-      ext_modules=[compiled_utils],
-      package_data=pkg_data,
-      classifiers=['Intended Audience :: Science/Research',
-                   'Topic :: Scientific/Engineering',
-                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-                   'Programming Language :: Python :: 3'])
+setup(ext_modules=[compiled_utils],
+      package_data=pkg_data)
