911c9257c137409cda2dce0c54fe8986bc038ad0
tobiscode <47008700+tobiscode@users.noreply.github.com>
2021-04-26 20:33:10 -0700

    Version bump to 0.6

--- geonat/__init__.py
+++ geonat/__init__.py
@@ -36,7 +36,7 @@ from .station import Station
 from .network import Network
 
 # package version
-__version__ = '0.5.1'
+__version__ = '0.6'
 
 # preparational steps
 multiprocessing.set_start_method('spawn', True)
