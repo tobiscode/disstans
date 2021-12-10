22da1c9ee676b88ede4ed503bb00f489cc8548d1
tobiscode <47008700+tobiscode@users.noreply.github.com>
2021-12-10 09:51:56 -0800

    add favicon

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -66,6 +66,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# add favicon
+html_favicon = '_static/favicon.ico'
+
 # -- Other settings ------------------------------------------------
 
 # autodoc settings
