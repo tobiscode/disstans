1d1ad581ea35e01d4930f4aff35aff45734c8ba5
tobiscode <47008700+tobiscode@users.noreply.github.com>
2022-10-25 17:30:03 -0700

    Bump version to 1.2-beta

--- disstans/__init__.py
+++ disstans/__init__.py
@@ -35,7 +35,7 @@ from .station import Station
 from .network import Network
 
 # package version
-__version__ = '1.1'
+__version__ = '1.2-beta'
 
 # preparational steps
 multiprocessing.set_start_method('spawn', True)
