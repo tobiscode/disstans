7c8b82da2d032bb36934089d2a9db6bfe228091e
tobiscode <47008700+tobiscode@users.noreply.github.com>
2023-08-11 10:58:13 -0700

    update conf.py and Makefile to use sphinx_autodoc_typehints and do incremental docs copy

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -14,9 +14,9 @@ import os
 import sys
 import datetime
 import sphinx_rtd_theme  # noqa: F401
+import sphinx_autodoc_typehints  # noqa: F401
 sys.path.insert(0, os.path.abspath('..'))
 from disstans import __version__ as disstans_version  # noqa: E402
-
 
 # -- Project information -----------------------------------------------------
 
@@ -28,7 +28,6 @@ version = disstans_version
 # The full version, including alpha/beta/rc tags
 release = disstans_version
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -38,7 +37,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
               'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
               'sphinx_rtd_theme', 'sphinx.ext.intersphinx',
               'sphinx.ext.mathjax', 'sphinx.ext.autosectionlabel',
-              'sphinxcontrib.video']
+              'sphinxcontrib.video', 'sphinx_autodoc_typehints']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,9 +49,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # complain about broken links, check every once in a while
 nitpicky = True
-nitpick_ignore = [('py:class', 'optional'), ('py:class', 'function'),
-                  ('py:class', 'iterable')]
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -80,12 +76,12 @@ autodoc_default_options = {'undoc-members': True,
                            'member-order': 'groupwise'}
 
 # intersphinx settings
-intersphinx_mapping = {'python': ('https://docs.python.org/3.10/', None),
-                       'numpy': ('https://numpy.org/doc/1.24/', None),
-                       'scipy': ('https://docs.scipy.org/doc/scipy-1.10.1/', None),
-                       'pandas': ('https://pandas.pydata.org/pandas-docs/version/1.5.3/', None),
-                       'matplotlib': ('https://matplotlib.org/3.7.0/', None),
-                       'scikit-learn': ('https://scikit-learn.org/1.2/', None)}
+intersphinx_mapping = {'python': ('https://docs.python.org/3.11/', None),
+                       'numpy': ('https://numpy.org/doc/1.25/', None),
+                       'scipy': ('https://docs.scipy.org/doc/scipy-1.11.1/', None),
+                       'pandas': ('https://pandas.pydata.org/pandas-docs/version/2.0.3/', None),
+                       'matplotlib': ('https://matplotlib.org/3.7.2/', None),
+                       'scikit-learn': ('https://scikit-learn.org/1.3/', None)}
 
 # ReadTheDocs theme settings
 html_theme_options = {'collapse_navigation': False,
@@ -93,6 +89,9 @@ html_theme_options = {'collapse_navigation': False,
 
 # allow the reusing of 'Classes' and 'Functions' section labels by prefixing the document name
 autosectionlabel_prefix_document = True
+
+# typehints settings
+typehints_defaults = "comma"
 
 # add copybutton.js (https://github.com/readthedocs/sphinx_rtd_theme/issues/167)
 def setup(app):  # noqa: E302
