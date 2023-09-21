051bdee19fb3afc2260fcdf7e7c7bd15de6f25ec
tobiscode <47008700+tobiscode@users.noreply.github.com>
2023-09-21 10:24:27 -0700

    Bump version to 2.0 RC0

--- disstans/__init__.py
+++ disstans/__init__.py
@@ -35,7 +35,7 @@ from .station import Station
 from .network import Network
 
 # package version
-__version__ = '2.0b0'
+__version__ = '2.0rc0'
 
 # preparational steps
 multiprocessing.set_start_method('spawn', True)
